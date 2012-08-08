3204ba71358fd631415e5e503b79aa3f2c4cdb83
Sean O'Neil <oneilse14@gmail.com>
2012-08-08 16:52:53 -0400

    Cleaned up NodeIndexService a lot. Added a ton of Javadocs as well

--- src/com/atami/mgodroid/io/NodeIndexService.java
+++ src/com/atami/mgodroid/io/NodeIndexService.java
@@ -1,102 +1,113 @@
 package com.atami.mgodroid.io;
 
-import java.util.ArrayList;
+import java.io.IOException;
+import java.net.MalformedURLException;
 import java.util.Iterator;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import android.app.IntentService;
 import android.content.ContentResolver;
 import android.content.ContentValues;
 import android.content.Context;
 import android.content.Intent;
 import android.database.Cursor;
-import android.os.Bundle;
-import android.os.ResultReceiver;
-import android.util.Log;
 
 import com.atami.mgodroid.provider.NodeIndexProvider;
-import com.atami.mgodroid.util.BlockingIntentService;
-import com.atami.mgodroid.util.DetachableResultReceiver;
 
-public class NodeIndexService extends BlockingIntentService {
+public class NodeIndexService extends IntentService {
 
-	private static String TAG = "NodeIndexService";
+	//private static String TAG = "NodeIndexService";
 
 	public static String GET_NEXT_PAGE = "get_next_page";
 	public static String REFRESH = "node_index_refresh";
 	public static String TYPE = "node_index_type";
 
-	public static String RESULT = "result";
-	public static String RESULT_RECEIVER = "receiver";
 	public static final int STATUS_ERROR = 0;
 	public static final int STATUS_COMPLETE = 1;
 	public static final int STATUS_RUNNING = 2;
 
-	private ResultReceiver mReceiver;
+	private int nodeIndexType;
 
 	public NodeIndexService() {
 		super("NodeIndexService");
 	}
 
 	@Override
-	protected void onHandleBlockingIntent(Intent intent) {
+	protected void onHandleIntent(Intent intent) {
 		String action = intent.getAction();
-		String indexType = intent.getStringExtra(TYPE);
-		mReceiver = intent.getParcelableExtra(RESULT_RECEIVER);
-		mReceiver.send(STATUS_RUNNING, Bundle.EMPTY);
-		Log.d(TAG, action + " running");
+		nodeIndexType = intent.getIntExtra(TYPE, -1);
+		// mReceiver.send(STATUS_RUNNING, Bundle.EMPTY);
+		// Log.d(TAG, action + " running");
 
-		if (action.equals(GET_NEXT_PAGE)) {
-			getNextNodeIndexPage(indexType);
-		} else if (action.equals(REFRESH)) {
-			refreshIndexPage(indexType);
+		try {
+			if (action.equals(GET_NEXT_PAGE)) {
+				JSONArray index;
+				index = API.getNodeIndex(nodeIndexType, getNextPageNumber(),
+						false);
+				insertNodeIndex(index);
+			} else if (action.equals(REFRESH)) {
+				JSONArray index = API.getNodeIndex(nodeIndexType, 0, false);
+				dropNodeIndex();
+				insertNodeIndex(index);
+			}
+		} catch (MalformedURLException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		} catch (JSONException e) {
+			e.printStackTrace();
 		}
 
-		mReceiver.send(STATUS_COMPLETE, Bundle.EMPTY);
-		Log.d(TAG, action + " completed");
+		// mReceiver.send(STATUS_COMPLETE, Bundle.EMPTY);
+		// Log.d(TAG, action + " completed");
 	}
 
-	// Drops all rows where node_type is type. This should be
-	// used when a user refreshes a NodeIndexListFragment and
-	// needs all fresh data
-	private void dropNodeIndex(String type) {
+	/**
+	 * Drops all rows associated with the current node index type.
+	 */
+	private void dropNodeIndex() {
 		ContentResolver cr = getContentResolver();
-		cr.delete(NodeIndexProvider.CONTENT_URI, "node_index_type = ?",
-				new String[] { type });
+		cr.delete(NodeIndexProvider.CONTENT_URI,
+				NodeIndexProvider.WHERE[nodeIndexType],
+				NodeIndexProvider.WHERE_ARGS[nodeIndexType]);
 	}
 
-	// Returns the page number to retrieve from MGoBlog based on
-	// what is currently in the database.
-	private int getMaxPage(String type) {
+	/**
+	 * Queries the Node Index Content Provider for the current count of rows for
+	 * the current node index type. This value is divided by the page size (20)
+	 * to get the value of the next page number to retrieve.
+	 * 
+	 * @return the next page number to retrieve
+	 */
+	private int getNextPageNumber() {
 		ContentResolver cr = getContentResolver();
 		Cursor c = cr.query(NodeIndexProvider.CONTENT_URI,
-				new String[] {"count(*) AS count"}, "type = ?", new String[] {type}, null);
+				new String[] { "count(*) AS count" },
+				NodeIndexProvider.WHERE[nodeIndexType],
+				NodeIndexProvider.WHERE_ARGS[nodeIndexType], null);
 		c.moveToFirst();
-		return c.getInt(0)/20;
+		return c.getInt(0) / 20;
 	}
 
-	private JSONArray getNodeIndexPage(int page, String type) {
-		JSONArray index = new JSONArray();
-		try {
-			index = API.getNodeIndex(page, page, false);
-			Log.v(TAG, index.toString());
-		} catch (Exception e) {
-			e.printStackTrace();
-			mReceiver.send(STATUS_ERROR, Bundle.EMPTY);
-		}
-		return index;
-	}
-
-	private void insertNodeIndex(JSONArray index, int page) throws JSONException {
+	/**
+	 * Iterates over each object in the JSONArray index, creating
+	 * a new row for each object. Each object is iterated over its
+	 * own fields, using the field names as the key in the content
+	 * provider row.
+	 * @param index JSONArray of node index JSONObjects
+	 * @throws JSONException
+	 */
+	private void insertNodeIndex(JSONArray index) throws JSONException {
 		ContentResolver cr = getContentResolver();
 		for (int i = 0; i < index.length(); i++) {
 			ContentValues cv = new ContentValues();
 			JSONObject o = index.getJSONObject(i);
 			Iterator<?> keys = o.keys();
-			
-			while(keys.hasNext()){
+
+			while (keys.hasNext()) {
 				String key = (String) keys.next();
 				cv.put(key, o.getString(key));
 			}
@@ -104,50 +115,27 @@ public class NodeIndexService extends BlockingIntentService {
 		}
 	}
 
-	// Gets the next node index page given the type, and
-	// inserts it into the database.
-	private void getNextNodeIndexPage(String type) {
-		int page = getMaxPage(type);
-		JSONArray index = getNodeIndexPage(page, type);
-		try {
-			insertNodeIndex(index, page);
-		} catch (JSONException e) {
-			e.printStackTrace();
-		}
-	}
-
-	// Gets the first node index page given the type, and
-	// if successful drops all rows for that type and inserts
-	// the retrieved page.
-	private void refreshIndexPage(String type) {
-		JSONArray index = getNodeIndexPage(0, type);
-		if (index.length() != 0) {
-			//dropNodeIndex(type);
-			try {
-				insertNodeIndex(index, 0);
-			} catch (JSONException e) {
-				e.printStackTrace();
-			}
-		}
-	}
-
-	// Helper methods for using the service
-
-	public static void refreshNodeIndex(String indexType, Context context,
-			DetachableResultReceiver receiver) {
+	/**
+	 * Helper method for refreshing the rows for a given node index
+	 * @param nodeIndexType an int constant identifying the node index
+	 * @param context used to start the IntentService
+	 */
+	public static void refreshNodeIndex(int nodeIndexType, Context context) {
 		final Intent i = new Intent(context, NodeIndexService.class);
 		i.setAction(NodeIndexService.REFRESH);
-		i.putExtra(NodeIndexService.TYPE, indexType);
-		i.putExtra(NodeIndexService.RESULT_RECEIVER, receiver);
+		i.putExtra(TYPE, nodeIndexType);
 		context.startService(i);
 	}
 
-	public static void getNextNodeIndexPage(String indexType, Context context,
-			DetachableResultReceiver receiver) {
+	/**
+	 * Helper method for getting the next page of rows for a given node index
+	 * @param indexType an int constant identifying the node index
+	 * @param context used to start the IntentService
+	 */
+	public static void getNextNodeIndexPage(String indexType, Context context) {
 		Intent i = new Intent(context, NodeIndexService.class);
 		i.setAction(NodeIndexService.GET_NEXT_PAGE);
 		i.putExtra(NodeIndexService.TYPE, indexType);
-		i.putExtra(NodeIndexService.RESULT_RECEIVER, receiver);
 		context.startService(i);
 	}
 }
