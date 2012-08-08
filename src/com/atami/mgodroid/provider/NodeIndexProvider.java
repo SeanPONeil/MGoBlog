aeab4e7b51ed96b47d564633d68262c4a1af059e
Sean O'Neil <oneilse14@gmail.com>
2012-08-08 16:31:00 -0400

    Added where and whereargs constants to NodeIndexProvider to remove duplicate code

--- src/com/atami/mgodroid/provider/NodeIndexProvider.java
+++ src/com/atami/mgodroid/provider/NodeIndexProvider.java
@@ -4,6 +4,10 @@ import it.mmo.classcontentprovider.ClassContentProvider;
 import android.net.Uri;
 
 public class NodeIndexProvider extends ClassContentProvider {
+	
+	public static final String[] WHERE = { "type = ?", "promote = ?", "type = ?", "type = ?" };
+	public static final String[][] WHERE_ARGS = { new String[] {"forum"}, new String[] {"1"}, new String[] {"blog"}, new String[] {"link"} };
+	
 	public static final Uri CONTENT_URI = Uri.parse("content://mgoblog.provider/node_indices");
 	public NodeIndexProvider() {
 		provideClass(NodeIndex.class);
