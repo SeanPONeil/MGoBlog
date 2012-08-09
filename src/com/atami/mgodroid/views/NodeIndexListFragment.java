123187dfef8f4c563822903ef2681169c668cfef
Sean O'Neil <oneilse14@gmail.com>
2012-08-09 15:27:03 -0400

    Integrated the PTR EndOfListListener for pulling in the next node index page. So much better than the hacks from before

--- src/com/atami/mgodroid/views/NodeIndexListFragment.java
+++ src/com/atami/mgodroid/views/NodeIndexListFragment.java
@@ -9,16 +9,13 @@ import android.support.v4.content.Loader;
 import android.support.v4.widget.SimpleCursorAdapter;
 import android.text.format.DateUtils;
 import android.util.Log;
-import android.view.LayoutInflater;
 import android.view.View;
-import android.view.ViewGroup;
 import android.widget.AbsListView;
 import android.widget.AbsListView.OnScrollListener;
 import android.widget.ListView;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
-import com.atami.mgodroid.R;
 import com.atami.mgodroid.io.NodeIndexService;
 import com.atami.mgodroid.io.StatusEvents.NodeIndexStatus;
 import com.atami.mgodroid.io.StatusEvents.Status;
@@ -26,11 +23,12 @@ import com.atami.mgodroid.provider.NodeIndexProvider;
 import com.atami.mgodroid.util.BusProvider;
 import com.atami.mgodroid.util.SherlockPullToRefreshListFragment;
 import com.handmark.pulltorefresh.library.PullToRefreshBase;
+import com.handmark.pulltorefresh.library.PullToRefreshBase.OnLastItemVisibleListener;
 import com.handmark.pulltorefresh.library.PullToRefreshBase.OnRefreshListener;
 import com.squareup.otto.Subscribe;
 
 public class NodeIndexListFragment extends SherlockPullToRefreshListFragment implements
-		LoaderCallbacks<Cursor>, OnScrollListener, OnRefreshListener<ListView> {
+		LoaderCallbacks<Cursor>, OnRefreshListener<ListView>, OnLastItemVisibleListener{
 
 	// This is the Adapter being used to display the list's data.
 	SimpleCursorAdapter mAdapter;
@@ -60,7 +58,7 @@ public class NodeIndexListFragment extends SherlockPullToRefreshListFragment imp
 
 	public static NodeIndexListFragment newInstance(int type) {
 		NodeIndexListFragment f = new NodeIndexListFragment();
-
+		
 		// Supply index input as an argument.
 		Bundle args = new Bundle();
 		args.putInt("NodeIndexType", type);
@@ -108,7 +106,7 @@ public class NodeIndexListFragment extends SherlockPullToRefreshListFragment imp
 		super.onActivityCreated(savedInstanceState);
 		setHasOptionsMenu(true);
 
-		getListView().setOnScrollListener(this);
+		getPullToRefreshListView().setOnLastItemVisibleListener(this);
 		getPullToRefreshListView().setOnRefreshListener(this);
 
 		mAdapter = new SimpleCursorAdapter(getActivity(),
@@ -129,25 +127,6 @@ public class NodeIndexListFragment extends SherlockPullToRefreshListFragment imp
 		NodeIndexService.refreshNodeIndex(nodeIndexType, getActivity());
 	}
 
-//	@Override
-//	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-//		inflater.inflate(R.menu.node_index, menu);
-//		super.onCreateOptionsMenu(menu, inflater);
-//	}
-//
-//	@Override
-//	public boolean onOptionsItemSelected(MenuItem item) {
-//		switch (item.getItemId()) {
-//		case R.id.refresh:
-//			NodeIndexService.refreshNodeIndex(nodeIndexType, getActivity());
-//			getListView().setSelection(0);
-//			break;
-//		default:
-//			super.onOptionsItemSelected(item);
-//		}
-//		return true;
-//	}
-
 	@Override
 	public Loader<Cursor> onCreateLoader(int id, Bundle args) {
 		return new CursorLoader(getActivity(), NodeIndexProvider.CONTENT_URI,
@@ -165,29 +144,10 @@ public class NodeIndexListFragment extends SherlockPullToRefreshListFragment imp
 	public void onLoaderReset(Loader<Cursor> loader) {
 		mAdapter.swapCursor(null);
 	}
-
+	
 	@Override
-	public void onScroll(AbsListView list, int firstVisible, int visibleCount,
-			int totalCount) {
-
-		boolean atEndOfList = firstVisible + visibleCount >= totalCount;
-		boolean notAtEndOfList = firstVisible + visibleCount < totalCount;
-
-		if (getNextPageRunning && notAtEndOfList) {
-			getNextPageRunning = false;
-		}
-
-		if (!getNextPageRunning && atEndOfList && list.getChildCount() != 0) {
-			// Launch Intent Service to get next page
-			Log.d("test", "reached end of list");
-			NodeIndexService.getNextNodeIndexPage(nodeIndexType, getActivity());
-			getNextPageRunning = true;
-		}
-
-	}
-
-	@Override
-	public void onScrollStateChanged(AbsListView view, int scrollState) {
+	public void onLastItemVisible() {
+		NodeIndexService.getNextNodeIndexPage(nodeIndexType, getActivity());
 	}
 
 	@Subscribe
@@ -218,4 +178,5 @@ public class NodeIndexListFragment extends SherlockPullToRefreshListFragment imp
 			
 		});
 	}
+
 }
