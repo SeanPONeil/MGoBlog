d09b7c3198a3e3bdea8acc72573b670cd4a566c9
Sean O'Neil <oneilse14@gmail.com>
2012-08-08 22:56:53 -0400

    Fixed weird ViewPagerIndicator layout issue

--- src/com/atami/mgodroid/views/MGoBlogActivity.java
+++ src/com/atami/mgodroid/views/MGoBlogActivity.java
@@ -1,11 +1,9 @@
 package com.atami.mgodroid.views;
 
-import java.util.ArrayList;
-
-import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.ViewPager;
@@ -13,18 +11,20 @@ import android.util.Log;
 import android.widget.FrameLayout;
 
 import com.actionbarsherlock.app.ActionBar;
-import com.actionbarsherlock.app.ActionBar.Tab;
 import com.actionbarsherlock.app.SherlockFragmentActivity;
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
 import com.atami.mgodroid.R;
+import com.viewpagerindicator.PageIndicator;
+import com.viewpagerindicator.TitlePageIndicator;
 
 public class MGoBlogActivity extends SherlockFragmentActivity implements
 		NodeIndexListFragment.OnNodeIndexItemClickListener {
 
 	// Left pane
-	TabsAdapter mTabsAdapter;
 	ViewPager mPager;
+	PageIndicator mIndicator;
+	NodeIndexListFragmentAdapter mAdapter;
 
 	// Right pane
 	FrameLayout nodeFrame;
@@ -32,29 +32,20 @@ public class MGoBlogActivity extends SherlockFragmentActivity implements
 	// Whether or not we are in dual-pane mode
 	boolean mIsDualPane = false;
 
-	String[] NODE_INDEX_TYPES = { "forum", "story", "blog", "link" };
-	String[] NODE_INDEX_TITLES = { "Board", "Blog", "Diaries", "mgo.licio.us" };
-
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.main_layout);
-
-		final ActionBar bar = getSupportActionBar();
-		bar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
-
+		
 		mPager = (ViewPager) findViewById(R.id.NodeIndexViewPager);
-		mTabsAdapter = new TabsAdapter(this, mPager);
+		mIndicator = (TitlePageIndicator) findViewById(R.id.indicator);
 		nodeFrame = (FrameLayout) findViewById(R.id.NodeFrame);
+		
+		mAdapter = new NodeIndexListFragmentAdapter(getSupportFragmentManager());
+		mPager.setAdapter(mAdapter);
+		mIndicator.setViewPager(mPager);
 
 		mIsDualPane = getResources().getBoolean(R.bool.has_two_panes);
-
-		for (int i = 0; i < NODE_INDEX_TYPES.length; i++) {
-			Bundle args = new Bundle();
-			args.putString("node_index_type", NODE_INDEX_TYPES[i]);
-			mTabsAdapter.addTab(bar.newTab().setText(NODE_INDEX_TITLES[i]),
-					NodeIndexListFragment.class, args);
-		}
 	}
 
 	@Override
@@ -78,86 +69,28 @@ public class MGoBlogActivity extends SherlockFragmentActivity implements
 		inflater.inflate(R.menu.mgoblog, menu);
 		return true;
 	}
+	
+	private class NodeIndexListFragmentAdapter extends FragmentPagerAdapter{
+		
+		protected final String[] NODE_INDEX_TITLES = { "MGoBoard", "MGoBlog", "Diaries", "mgo.licio.us" };
 
-	public static class TabsAdapter extends FragmentPagerAdapter implements
-			ActionBar.TabListener, ViewPager.OnPageChangeListener {
-		private final Context mContext;
-		private final ActionBar mActionBar;
-		private final ViewPager mViewPager;
-		private final ArrayList<TabInfo> mTabs = new ArrayList<TabInfo>();
+	    public NodeIndexListFragmentAdapter(FragmentManager fm) {
+	        super(fm);
+	    }
 
-		static final class TabInfo {
-			private final Class<?> clss;
-			private final Bundle args;
+	    @Override
+	    public Fragment getItem(int position) {
+	        return NodeIndexListFragment.newInstance(position);
+	    }
 
-			TabInfo(Class<?> _class, Bundle _args) {
-				clss = _class;
-				args = _args;
-			}
-		}
-
-		public TabsAdapter(SherlockFragmentActivity activity, ViewPager pager) {
-			super(activity.getSupportFragmentManager());
-			mContext = activity;
-			mActionBar = activity.getSupportActionBar();
-			mViewPager = pager;
-			mViewPager.setAdapter(this);
-			mViewPager.setOnPageChangeListener(this);
-		}
-
-		public void addTab(ActionBar.Tab tab, Class<?> clss, Bundle args) {
-			TabInfo info = new TabInfo(clss, args);
-			tab.setTag(info);
-			tab.setTabListener(this);
-			mTabs.add(info);
-			mActionBar.addTab(tab);
-			notifyDataSetChanged();
-		}
-
-		public int getCount() {
-			return mTabs.size();
-		}
-
-		public NodeIndexListFragment getItem(int position) {
-			TabInfo info = mTabs.get(position);
-			NodeIndexListFragment f = (NodeIndexListFragment) Fragment
-					.instantiate(mContext, info.clss.getName(), info.args);
-			return f;
-		}
-
-		public void onPageScrolled(int position, float positionOffset,
-				int positionOffsetPixels) {
-		}
-
-		public void onPageSelected(int position) {
-			mActionBar.setSelectedNavigationItem(position);
-		}
-
-		public void onPageScrollStateChanged(int state) {
-		}
-
-		public void onTabSelected(Tab tab, FragmentTransaction ft) {
-			mViewPager.setCurrentItem(tab.getPosition());
-			// Log.v(TAG, "clicked");
-			Object tag = tab.getTag();
-			for (int i = 0; i < mTabs.size(); i++) {
-				if (mTabs.get(i) == tag) {
-					mViewPager.setCurrentItem(i);
-				}
-			}
-		}
-
-		public void onTabUnselected(Tab tab, FragmentTransaction ft) {
-		}
-
-		public void onTabReselected(Tab tab, FragmentTransaction ft) {
-		}
-
-		public void onTabReselected(Tab tab, android.app.FragmentTransaction ft) {
-		}
-
-		public void onTabUnselected(Tab tab, android.app.FragmentTransaction ft) {
-		}
+	    @Override
+	    public int getCount() {
+	        return NODE_INDEX_TITLES.length;
+	    }
+	    
+	    @Override
+	    public CharSequence getPageTitle(int position) {
+	        return NODE_INDEX_TITLES[position];
+	    }
 	}
-
 }
