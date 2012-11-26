f0fef1608a315a023e40bb4bb5a6c65fa7b72aec
Sean O'Neil <oneilse14@gmail.com>
2012-11-26 18:37:26 -0500

    Removed @Produce from NodeIndexCache since each fragment has access to the Singleton

--- MGoBlog/src/main/java/com/atami/mgodroid/core/NodeIndexCache.java
+++ MGoBlog/src/main/java/com/atami/mgodroid/core/NodeIndexCache.java
@@ -2,11 +2,8 @@ package com.atami.mgodroid.core;
 
 import com.atami.mgodroid.MGoBlogApplication;
 import com.atami.mgodroid.core.MGoBlogAPIModule.MGoBlogAPI;
-import com.atami.mgodroid.core.events.NodeIndexRefreshEvent;
 import com.atami.mgodroid.core.events.NodeIndexUpdateEvent;
 import com.squareup.otto.Bus;
-import com.squareup.otto.Produce;
-import com.squareup.otto.Subscribe;
 
 import java.util.Collections;
 import java.util.HashMap;
@@ -30,45 +27,25 @@ public class NodeIndexCache {
         return nodeIndexes;
     }
 
-    //Tracker for the type of the last updated Node Index
-    private String lastUpdatedNodeIndex;
-
     public NodeIndexCache(Bus bus, MGoBlogAPI api) {
         this.bus = bus;
         this.api = api;
         bus.register(this);
         nodeIndexes = Collections.synchronizedMap(new HashMap<String, List<NodeIndex>>());
-        updateNodeIndexFromDisk(MGoBlogApplication.nodeIndexTypes);
+        updateNodeIndexesFromDisk(MGoBlogApplication.nodeIndexTypes);
     }
 
-    private void updateNodeIndexFromDisk(final String[] nodeIndexTypes) {
+    private void updateNodeIndexesFromDisk(final String[] nodeIndexTypes) {
         new Thread(new Runnable() {
             @Override
             public void run() {
                 for (String type : nodeIndexTypes) {
                     List<NodeIndex> list = NodeIndex.getAll(type);
                     nodeIndexes.put(type, list);
-                    lastUpdatedNodeIndex = type;
-                    bus.post(produceNodeIndexUpdateEvent());
+                    bus.post(new NodeIndexUpdateEvent(type));
                 }
             }
         }).start();
-    }
-
-    private void updateNodeIndexFromDisk(final String type) {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                List<NodeIndex> list = NodeIndex.getAll(type);
-                nodeIndexes.put(type, list);
-                bus.post(produceNodeIndexUpdateEvent());
-            }
-        }).start();
-    }
-
-    @Produce
-    public NodeIndexUpdateEvent produceNodeIndexUpdateEvent() {
-        return new NodeIndexUpdateEvent(lastUpdatedNodeIndex);
     }
 
     public synchronized void refreshNodeIndex(final String type, final String page){
@@ -76,10 +53,10 @@ public class NodeIndexCache {
             @Override
             public void run() {
                 List<NodeIndex> list = api.getNodeIndex(type, page, "0");
-                if (page == "0") {
+                if (page.equals("0")) {
                     nodeIndexes.remove(type);
                     nodeIndexes.put(type, list);
-                    bus.post(produceNodeIndexUpdateEvent());
+                    bus.post(new NodeIndexUpdateEvent(type));
                     NodeIndex.deleteAll(type);
                     for(NodeIndex ni: list){
                         ni.save();
@@ -88,34 +65,7 @@ public class NodeIndexCache {
                     List<NodeIndex> currentList = nodeIndexes.get(type);
                     currentList.addAll(list);
                     nodeIndexes.put(type, currentList);
-                    bus.post(produceNodeIndexUpdateEvent());
-                    for(NodeIndex ni: currentList){
-                        ni.save();
-                    }
-                }
-            }
-        }).start();
-    }
-
-    @Subscribe
-    public void onNodeIndexRefreshEvent(final NodeIndexRefreshEvent event) {
-        new Thread(new Runnable() {
-            @Override
-            public void run() {
-                List<NodeIndex> list = api.getNodeIndex(event.type, event.page, "0");
-                if (event.page == "0") {
-                    nodeIndexes.remove(event.type);
-                    nodeIndexes.put(event.type, list);
-                    bus.post(produceNodeIndexUpdateEvent());
-                    NodeIndex.deleteAll(event.type);
-                    for(NodeIndex ni: list){
-                        ni.save();
-                    }
-                }else{
-                    List<NodeIndex> currentList = nodeIndexes.get(event.type);
-                    currentList.addAll(list);
-                    nodeIndexes.put(event.type, currentList);
-                    bus.post(produceNodeIndexUpdateEvent());
+                    bus.post(new NodeIndexUpdateEvent(type));
                     for(NodeIndex ni: currentList){
                         ni.save();
                     }
