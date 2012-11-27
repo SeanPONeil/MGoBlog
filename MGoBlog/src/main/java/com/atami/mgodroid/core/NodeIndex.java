187cfb9929e224c00b0bf0db5b8b39fdebb1ab0b
Sean O'Neil <oneilse14@gmail.com>
2012-11-26 21:40:27 -0500

    Got rid of unused getter/setters and removed warnings from Intellij

--- MGoBlog/src/main/java/com/atami/mgodroid/core/NodeIndex.java
+++ MGoBlog/src/main/java/com/atami/mgodroid/core/NodeIndex.java
@@ -5,14 +5,8 @@ import com.activeandroid.annotation.Column;
 import com.activeandroid.annotation.Table;
 import com.activeandroid.query.Delete;
 import com.activeandroid.query.Select;
-import com.activeandroid.serializer.TypeSerializer;
-import com.google.gson.Gson;
-import org.joda.time.DateTime;
 
-import java.util.Date;
 import java.util.List;
-
-import static com.activeandroid.util.SQLiteUtils.SQLiteType;
 
 @Table(name = "NodeIndexes")
 public class NodeIndex extends Model {
@@ -85,132 +79,11 @@ public class NodeIndex extends Model {
         return nid;
     }
 
-    public void setNid(int nid) {
-        this.nid = nid;
-    }
-
-    public int getVid() {
-        return vid;
-    }
-
-    public void setVid(int vid) {
-        this.vid = vid;
-    }
-
-    public String getType() {
-        return type;
-    }
-
-    public void setType(String type) {
-        this.type = type;
-    }
-
-    public String getLanguage() {
-        return language;
-    }
-
-    public void setLanguage(String language) {
-        this.language = language;
-    }
-
     public String getTitle() {
         return title;
     }
 
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public int getUid() {
-        return uid;
-    }
-
-    public void setUid(int uid) {
-        this.uid = uid;
-    }
-
-    public int getStatus() {
-        return status;
-    }
-
-    public void setStatus(int status) {
-        this.status = status;
-    }
-
     public long getCreated() {
         return created;
-    }
-
-    public void setCreated(long created) {
-        this.created = created;
-    }
-
-    public long getChanged() {
-        return changed;
-    }
-
-    public void setChanged(long changed) {
-        this.changed = changed;
-    }
-
-    public int getComment() {
-        return comment;
-    }
-
-    public void setComment(int comment) {
-        this.comment = comment;
-    }
-
-    public int getPromote() {
-        return promote;
-    }
-
-    public void setPromote(int promote) {
-        this.promote = promote;
-    }
-
-    public int getModerate() {
-        return moderate;
-    }
-
-    public void setModerate(int moderate) {
-        this.moderate = moderate;
-    }
-
-    public int getSticky() {
-        return sticky;
-    }
-
-    public void setSticky(int sticky) {
-        this.sticky = sticky;
-    }
-
-    public int getTnid() {
-        return tnid;
-    }
-
-    public void setTnid(int tnid) {
-        this.tnid = tnid;
-    }
-
-    public int getTranslate() {
-        return translate;
-    }
-
-    public void setTranslate(int translate) {
-        this.translate = translate;
-    }
-
-    public String getUri() {
-        return uri;
-    }
-
-    public void setUri(String uri) {
-        this.uri = uri;
-    }
-
-    public String toJSON() {
-        Gson gson = new Gson();
-        return gson.toJson(this);
     }
 }
