9c58f435bc066138494ce6683bcfed81c08d5640
Sean O'Neil <oneilse14@gmail.com>
2013-01-30 22:57:52 -0500

    Initial MenuDrawer implementation for MGoBlogActivity

--- MGoBlog/src/main/java/com/atami/mgodroid/ui/MGoBlogActivity.java
+++ MGoBlog/src/main/java/com/atami/mgodroid/ui/MGoBlogActivity.java
@@ -1,66 +1,61 @@
 package com.atami.mgodroid.ui;
 
+import android.content.ClipData;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentTransaction;
 import android.util.SparseArray;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ArrayAdapter;
-import android.widget.SpinnerAdapter;
-import android.widget.TextView;
+import android.widget.*;
 import com.actionbarsherlock.app.ActionBar;
+import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuItem;
 import com.atami.mgodroid.R;
 import com.atami.mgodroid.models.NodeIndex;
 import com.atami.mgodroid.ui.base.BaseActivity;
 import com.squareup.otto.Subscribe;
+import net.simonvt.menudrawer.MenuDrawer;
+import net.simonvt.menudrawer.Position;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 import static com.actionbarsherlock.app.ActionBar.OnNavigationListener;
 
 public class MGoBlogActivity extends BaseActivity implements OnNavigationListener {
 
-    // Left pane
-    //ViewPager mPager;
+    private static final String STATE_ACTIVE_POSITION = "com.atami.mgodroid.activePosition";
 
-    // Whether or not we are in dual-pane mode
-    boolean mIsDualPane;
+    private MenuDrawer mMenuDrawer;
 
-    private SparseArray<Fragment> nodeIndexFragments = new SparseArray<Fragment>();
-
+    private int mActivePosition = -1;
+    private Handler mHandler;
+    private Runnable mToggleUpRunnable;
+    private boolean mDisplayUp = true;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main_layout);
-        getSupportActionBar().setDisplayShowTitleEnabled(false);
-        getSupportActionBar().setNavigationMode(ActionBar.NAVIGATION_MODE_LIST);
 
-        mIsDualPane = getResources().getBoolean(R.bool.has_two_panes);
-
-        SpinnerAdapter mSpinnerAdapter = new TitlesAdapter(this);
-        getSupportActionBar().setListNavigationCallbacks(mSpinnerAdapter, this);
-
-        if (savedInstanceState == null) {
-            getSupportActionBar().setSelectedNavigationItem(1);
-        } else {
-            getSupportActionBar().setSelectedNavigationItem(savedInstanceState.getInt("navigationIndex"));
+        if(savedInstanceState != null){
+            mActivePosition = savedInstanceState.getInt(STATE_ACTIVE_POSITION);
         }
+
+        mMenuDrawer = MenuDrawer.attach(this, Position.LEFT);
+        mMenuDrawer.setContentView(android.R.layout.simple_list_item_1);
+
+        buildMenuDrawer();
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
-        outState.putInt("navigationIndex", getSupportActionBar().getSelectedNavigationIndex());
         super.onSaveInstanceState(outState);
-    }
-
-    @Override
-    protected void onRestoreInstanceState(Bundle savedInstanceState) {
-        super.onRestoreInstanceState(savedInstanceState);
+        outState.putInt(STATE_ACTIVE_POSITION, mActivePosition);
     }
 
     @Override
@@ -87,47 +82,187 @@ public class MGoBlogActivity extends BaseActivity implements OnNavigationListene
 
     @Subscribe
     public void onNodeIndexItemClick(NodeIndex nodeIndex) {
-        if (mIsDualPane) {
-            NodeFragment nodeFragment = NodeFragment.newInstance(nodeIndex.getNid());
-            FragmentTransaction ft = getSupportFragmentManager()
-                    .beginTransaction();
-            ft.replace(R.id.NodeFrame, nodeFragment).commit();
-        } else {
-            Intent intent = new Intent(this, NodeActivity.class);
-            intent.putExtra("nid", nodeIndex.getNid());
-            startActivity(intent);
-        }
+        Intent intent = new Intent(this, NodeActivity.class);
+        intent.putExtra("nid", nodeIndex.getNid());
+        startActivity(intent);
     }
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
-
         switch (item.getItemId()) {
-
+            case android.R.id.home:
+                mMenuDrawer.toggleMenu();
+                return true;
         }
-
         return super.onOptionsItemSelected(item);
     }
 
-    private class TitlesAdapter extends ArrayAdapter<String> implements SpinnerAdapter {
+    @Override
+    public void onBackPressed() {
+        final int drawerState = mMenuDrawer.getDrawerState();
+        if (drawerState == MenuDrawer.STATE_OPEN || drawerState == MenuDrawer.STATE_OPENING) {
+            mMenuDrawer.closeMenu();
+            return;
+        }
 
-        public TitlesAdapter(Context context) {
-            super(context, android.R.layout.simple_list_item_1, Arrays.asList(context.getResources().getStringArray(R.array
-                    .node_index_titles)));
+        super.onBackPressed();
+    }
+
+    private void buildMenuDrawer(){
+        List<Object> items = new ArrayList<Object>();
+        items.add(new DrawerItem("Item 1", R.drawable.ic_action_refresh));
+        items.add(new DrawerItem("Item 2", R.drawable.ic_action_refresh));
+        items.add(new DrawerCategory("Cat 1"));
+        items.add(new DrawerItem("Item 3", R.drawable.ic_action_refresh));
+        items.add(new DrawerItem("Item 4", R.drawable.ic_action_refresh));
+        items.add(new DrawerCategory("Cat 2"));
+        items.add(new DrawerItem("Item 5", R.drawable.ic_action_refresh));
+        items.add(new DrawerItem("Item 6", R.drawable.ic_action_refresh));
+
+        ListView menuList = new ListView(this);
+        MenuDrawerAdapter menuAdapter = new MenuDrawerAdapter(items);
+        menuList.setAdapter(menuAdapter);
+
+        menuList.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                mActivePosition = position;
+                mMenuDrawer.setActiveView(view, position);
+                mMenuDrawer.closeMenu();
+            }
+        });
+
+        menuList.setOnScrollListener(new AbsListView.OnScrollListener() {
+            @Override
+            public void onScrollStateChanged(AbsListView absListView, int i) {
+            }
+
+            @Override
+            public void onScroll(AbsListView view, int firstVisibleItem, int visibleItemCount, int totalItemCount) {
+                mMenuDrawer.invalidate();
+            }
+        });
+
+        mMenuDrawer.setMenuView(menuList);
+
+        //"Up" button will blink until it is clicked
+        mHandler = new Handler();
+        mToggleUpRunnable = new Runnable() {
+            @Override
+            public void run() {
+                mDisplayUp = !mDisplayUp;
+                getActionBar().setDisplayHomeAsUpEnabled(mDisplayUp);
+                mHandler.postDelayed(mToggleUpRunnable, 500);
+            }
+        };
+
+        mHandler.postDelayed(mToggleUpRunnable, 500);
+
+        mMenuDrawer.setOnDrawerStateChangeListener(new MenuDrawer.OnDrawerStateChangeListener() {
+            @Override
+            public void onDrawerStateChange(int oldState, int newState) {
+                if (newState == MenuDrawer.STATE_OPEN) {
+                    mHandler.removeCallbacks(mToggleUpRunnable);
+                    if (!mDisplayUp) getActionBar().setDisplayHomeAsUpEnabled(true);
+                    mMenuDrawer.setOnDrawerStateChangeListener(null);
+                }
+            }
+        });
+    }
+
+    private static class DrawerItem {
+
+        String mTitle;
+        int mIconRes;
+
+        DrawerItem(String title, int iconRes) {
+            mTitle = title;
+            mIconRes = iconRes;
+        }
+    }
+
+    private static class DrawerCategory {
+
+        String mTitle;
+
+        DrawerCategory(String title) {
+            mTitle = title;
+        }
+    }
+
+    private class MenuDrawerAdapter extends BaseAdapter {
+
+        private List<Object> mItems;
+
+        MenuDrawerAdapter(List<Object> items) {
+            mItems = items;
+        }
+
+        @Override
+        public int getCount() {
+            return mItems.size();
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return mItems.get(position);
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return position;
+        }
+
+        @Override
+        public int getItemViewType(int position) {
+            return getItem(position) instanceof
+                    DrawerItem ? 0 : 1;
+        }
+
+        @Override
+        public int getViewTypeCount() {
+            return 2;
+        }
+
+        @Override
+        public boolean isEnabled(int position) {
+            return getItem(position) instanceof DrawerItem;
+        }
+
+        @Override
+        public boolean areAllItemsEnabled() {
+            return false;
         }
 
         @Override
         public View getView(int position, View convertView, ViewGroup parent) {
-            convertView = super.getView(position, convertView, parent);
-            TextView title = (TextView) convertView.findViewById(android.R.id.text1);
-            return convertView;
-        }
+            View v = convertView;
+            Object item = getItem(position);
 
-        @Override
-        public View getDropDownView(int position, View convertView, ViewGroup parent) {
-            convertView = super.getView(position, convertView, parent);
-            TextView title = (TextView) convertView.findViewById(android.R.id.text1);
-            return convertView;
+            if (item instanceof DrawerCategory) {
+                if (v == null) {
+                    v = getLayoutInflater().inflate(R.layout.menu_row_category, parent, false);
+                }
+
+                ((TextView) v).setText(((DrawerCategory) item).mTitle);
+
+            } else {
+                if (v == null) {
+                    v = getLayoutInflater().inflate(R.layout.menu_row_item, parent, false);
+                }
+
+                TextView tv = (TextView) v;
+                tv.setText(((DrawerItem) item).mTitle);
+                tv.setCompoundDrawablesWithIntrinsicBounds(((DrawerItem) item).mIconRes, 0, 0, 0);
+            }
+
+            v.setTag(R.id.mdActiveViewPosition, position);
+
+            if (position == mActivePosition) {
+                mMenuDrawer.setActiveView(v, position);
+            }
+
+            return v;
         }
     }
 }
