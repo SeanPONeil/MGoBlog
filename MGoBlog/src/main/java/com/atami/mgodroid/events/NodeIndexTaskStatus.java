c0a332932ce5f85ba55d123b1676c040380d4674
Sean O'Neil <oneilse14@gmail.com>
2013-02-02 18:01:31 -0500

    Add shell script for installing non Maven repositories in your local repository

--- MGoBlog/src/main/java/com/atami/mgodroid/events/NodeIndexTaskStatus.java
+++ MGoBlog/src/main/java/com/atami/mgodroid/events/NodeIndexTaskStatus.java
@@ -1,11 +1,14 @@
 package com.atami.mgodroid.events;
 
-public class NodeIndexStatusEvent {
+public class NodeIndexTaskStatus {
+
+    public String title;
 
     public boolean refreshing;
     public boolean gettingNextPage;
 
-    public NodeIndexStatusEvent(boolean refreshing, boolean gettingNextPage){
+    public NodeIndexTaskStatus(String title, boolean refreshing, boolean gettingNextPage){
+        this.title = title;
         this.refreshing = refreshing;
         this.gettingNextPage = gettingNextPage;
     }
---
+++ get_dependencies.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+mkdir tmp
+#Retrofit
+cd tmp
+git clone https://github.com/square/retrofit.git
+cd retrofit
+mvn clean install
+cd ..
+rm -rf retrofit
+#android-menudrawer
+git clone https://github.com/SimonVT/android-menudrawer.git
+cd android-menudrawer
+mvn clean install
+cd ..
+rm -rf retrofit
+#dagger
+git clone https://github.com/square/dagger.git
+cd dagger
+mvn clean install
+cd ..
+rm -rf dagger
+cd ..
+rm -rf tmp
+
