6d853a77581c393585871505cb5f25b6797edd0c
Kristin Boyer <boyerkr@umich.edu>
2013-02-10 15:09:59 -0800

    webview with card background

--- MGoBlog/src/main/java/com/atami/mgodroid/ui/NodeFragment.java
+++ MGoBlog/src/main/java/com/atami/mgodroid/ui/NodeFragment.java
@@ -7,7 +7,10 @@ import android.support.v4.app.LoaderManager;
 import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.webkit.WebSettings;
+import android.webkit.WebView;
+
 import com.actionbarsherlock.view.Menu;
 import com.actionbarsherlock.view.MenuInflater;
 import com.actionbarsherlock.view.MenuItem;
@@ -26,137 +29,151 @@ import com.squareup.tape.TaskQueue;
 import javax.inject.Inject;
 import java.util.List;
 
-public class NodeFragment extends WebViewFragment implements LoaderManager.LoaderCallbacks<List<Node>> {
+public class NodeFragment extends WebViewFragment implements
+		LoaderManager.LoaderCallbacks<List<Node>> {
 
-    public final static String TAG = "NodeFragment";
+	public final static String TAG = "NodeFragment";
 
-    // ID of the current node
-    int nid;
+	// ID of the current node
+	int nid;
 
-    @Inject
-    TaskQueue<NodeTask> queue;
+	@Inject
+	TaskQueue<NodeTask> queue;
 
-    private Menu nodeMenu;
+	private Menu nodeMenu;
 
-    public static NodeFragment newInstance(int nid) {
-        NodeFragment f = new NodeFragment();
+	public static NodeFragment newInstance(int nid) {
+		NodeFragment f = new NodeFragment();
 
-        Bundle args = new Bundle();
-        args.putInt("nid", nid);
-        f.setArguments(args);
+		Bundle args = new Bundle();
+		args.putInt("nid", nid);
+		f.setArguments(args);
 
-        return f;
-    }
+		return f;
+	}
 
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setHasOptionsMenu(true);
-        nid = getArguments().getInt("nid");
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setHasOptionsMenu(true);
+		nid = getArguments().getInt("nid");
 
-        if (savedInstanceState == null) {
-            queue.add(new NodeTask(nid, getTag()));
-        }
-    }
+		if (savedInstanceState == null) {
+			queue.add(new NodeTask(nid, getTag()));
+		}
+	}
 
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
+	@Override
+	public void onActivityCreated(Bundle savedInstanceState) {
+		super.onActivityCreated(savedInstanceState);
 
-        getWebView().getSettings().setJavaScriptEnabled(true);
-        getWebView().getSettings().setDefaultFontSize(16);
-        getWebView().getSettings().setPluginState(WebSettings.PluginState.ON);
-        getWebView().getSettings().setLayoutAlgorithm(WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
+		getWebView().getSettings().setJavaScriptEnabled(true);
+		getWebView().getSettings().setDefaultFontSize(16);
+		getWebView().getSettings().setPluginState(WebSettings.PluginState.ON);
+		getWebView().getSettings().setLayoutAlgorithm(
+				WebSettings.LayoutAlgorithm.NARROW_COLUMNS);
+		
+		getWebView().setBackgroundDrawable(getResources().getDrawable(R.drawable.comment_card));
+		getWebView().setBackgroundColor(0x00000000);
+		getWebView().setLayerType(WebView.LAYER_TYPE_SOFTWARE, null);
+		
+		
+		
+		// Bug in ActiveAndroid ModelLoader: Cached results in LoaderManager
+		// aren't updated
+		// when the ModelLoaders From changes
+		getActivity().getSupportLoaderManager().restartLoader(0, null, this);
+	}
 
-        //Bug in ActiveAndroid ModelLoader: Cached results in LoaderManager aren't updated
-        //when the ModelLoaders From changes
-        getActivity().getSupportLoaderManager().restartLoader(0, null, this);
-    }
+	@Subscribe
+	public void onNewNodeTaskStatus(NodeTaskStatus status) {
+		// System.out.println(status.tag);
+		// System.out.println(getTag());
+		if (getTag() != null) {
+			if (status.tag.equals(getTag())) {
+				if (status.running) {
+					setRefreshActionItemState(true);
+				} else {
+					setRefreshActionItemState(false);
+				}
+			}
+		}
+	}
 
-    @Subscribe
-    public void onNewNodeTaskStatus(NodeTaskStatus status) {
-        //System.out.println(status.tag);
-        //System.out.println(getTag());
-        if(getTag() != null){
-            if (status.tag.equals(getTag())) {
-                if (status.running) {
-                    setRefreshActionItemState(true);
-                } else {
-                    setRefreshActionItemState(false);
-                }
-            }
-        }
-    }
+	@Override
+	public Loader<List<Node>> onCreateLoader(int id, Bundle args) {
+		From query = new Select().from(Node.class).where("nid = ?", nid);
+		return new ModelLoader<Node>(getActivity(), query);
+	}
 
-    @Override
-    public Loader<List<Node>> onCreateLoader(int id, Bundle args) {
-        From query = new Select().from(Node.class).where("nid = ?", nid);
-        return new ModelLoader<Node>(getActivity(), query);
-    }
+	@Override
+	public void onLoadFinished(Loader<List<Node>> nodeLoader, List<Node> node) {
+		if (!node.isEmpty()) {
+			System.out.println(node.get(0).toString());
+			getWebView().loadDataWithBaseURL("file:///android_asset/",
+					node.get(0).getBody(), "text/html", "UTF-8", null);
+			getSherlockActivity().getSupportActionBar().setTitle(
+					node.get(0).getTitle());
+			getSherlockActivity().getSupportActionBar().setSubtitle(
+					"By " + node.get(0).getName() + " - "
+							+ node.get(0).getCommentCount() + " " + "comments");
+		}
+	}
 
-    @Override
-    public void onLoadFinished(Loader<List<Node>> nodeLoader, List<Node> node) {
-        if (!node.isEmpty()) {
-            System.out.println(node.get(0).toString());
-            getWebView().loadDataWithBaseURL("file:///android_asset/", node.get(0).getBody(), "text/html", "UTF-8",
-                    null);
-            getSherlockActivity().getSupportActionBar().setTitle(node.get(0).getTitle());
-            getSherlockActivity().getSupportActionBar().setSubtitle("By " + node.get(0).getName() + " - " + node.get(0)
-                    .getCommentCount() + " " + "comments");
-        }
-    }
+	@Override
+	public void onLoaderReset(Loader<List<Node>> nodeLoader) {
+	}
 
-    @Override
-    public void onLoaderReset(Loader<List<Node>> nodeLoader) {
-    }
+	public void setRefreshActionItemState(boolean refreshing) {
+		if (nodeMenu == null) {
+			return;
+		}
+		final MenuItem refreshItem = nodeMenu.findItem(R.id.refresh);
+		if (refreshItem != null) {
+			if (refreshing) {
+				LayoutInflater inflater = (LayoutInflater) getActivity()
+						.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+				View refreshView = inflater.inflate(R.layout.refresh_menu_item,
+						null);
+				refreshItem.setActionView(refreshView);
+			} else {
+				refreshItem.setActionView(null);
+			}
+		}
+	}
 
-    public void setRefreshActionItemState(boolean refreshing) {
-        if (nodeMenu == null) {
-            return;
-        }
-        final MenuItem refreshItem = nodeMenu.findItem(R.id.refresh);
-        if (refreshItem != null) {
-            if (refreshing) {
-                LayoutInflater inflater = (LayoutInflater) getActivity().getSystemService(Context
-                        .LAYOUT_INFLATER_SERVICE);
-                View refreshView = inflater.inflate(R.layout.refresh_menu_item, null);
-                refreshItem.setActionView(refreshView);
-            } else {
-                refreshItem.setActionView(null);
-            }
-        }
-    }
+	@Override
+	public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+		inflater.inflate(R.menu.node_body, menu);
+		nodeMenu = menu;
+		super.onCreateOptionsMenu(menu, inflater);
+	}
 
-    @Override
-    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        inflater.inflate(R.menu.node_body, menu);
-        nodeMenu = menu;
-        super.onCreateOptionsMenu(menu, inflater);
-    }
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+		switch (item.getItemId()) {
+		case R.id.refresh:
+			queue.add(new NodeTask(nid, getTag()));
+			return true;
+		case R.id.comments:
+			// Instantiate a new fragment.
+			NodeCommentFragment newFragment = NodeCommentFragment
+					.newInstance(nid);
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        switch (item.getItemId()) {
-            case R.id.refresh:
-                queue.add(new NodeTask(nid, getTag()));
-                return true;
-            case R.id.comments:
-                // Instantiate a new fragment.
-                NodeCommentFragment newFragment = NodeCommentFragment.newInstance(nid);
-
-                // Add the fragment to the activity, pushing this transaction
-                // on to the back stack.
-                FragmentTransaction ft = getActivity().getSupportFragmentManager().beginTransaction();
-                ft.setCustomAnimations(R.anim.fragment_slide_left_enter,
-                        R.anim.fragment_slide_left_exit,
-                        R.anim.fragment_slide_right_enter,
-                        R.anim.fragment_slide_right_exit);
-                ft.replace(R.id.node_pane, newFragment, getTag());
-                ft.addToBackStack(null);
-                ft.commit();
-                return true;
-            default:
-                return super.onOptionsItemSelected(item);
-        }
-    }
+			// Add the fragment to the activity, pushing this transaction
+			// on to the back stack.
+			FragmentTransaction ft = getActivity().getSupportFragmentManager()
+					.beginTransaction();
+			ft.setCustomAnimations(R.anim.fragment_slide_left_enter,
+					R.anim.fragment_slide_left_exit,
+					R.anim.fragment_slide_right_enter,
+					R.anim.fragment_slide_right_exit);
+			ft.replace(R.id.node_pane, newFragment, getTag());
+			ft.addToBackStack(null);
+			ft.commit();
+			return true;
+		default:
+			return super.onOptionsItemSelected(item);
+		}
+	}
 }
