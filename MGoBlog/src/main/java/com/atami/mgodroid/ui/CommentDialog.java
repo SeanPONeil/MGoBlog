ab4966607b3298e98bacc318707d643387bbe0f6
Kristin Boyer <boyerkr@umich.edu>
2013-02-12 20:45:55 -0800

    framework for comments in place

--- MGoBlog/src/main/java/com/atami/mgodroid/ui/CommentDialog.java
+++ MGoBlog/src/main/java/com/atami/mgodroid/ui/CommentDialog.java
@@ -1,7 +1,6 @@
 package com.atami.mgodroid.ui;
 
 import android.app.Activity;
-//import android.app.DialogFragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
