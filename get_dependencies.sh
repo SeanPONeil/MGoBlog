4df7ee0dd80efed31cf1def9b83d5b0d2c390ca8
Sean O'Neil <oneilse14@gmail.com>
2013-02-12 21:12:34 -0500

    Use MenuDrawer 2.0.1 from Maven Central

--- get_dependencies.sh
+++ get_dependencies.sh
@@ -7,13 +7,6 @@ cd retrofit
 mvn clean install
 cd ..
 rm -rf retrofit
-#android-menudrawer
-git clone https://github.com/SimonVT/android-menudrawer.git
-cd android-menudrawer
-git checkout -b dev origin/dev
-mvn clean install
-cd ..
-rm -rf retrofit
 #dagger
 git clone https://github.com/square/dagger.git
 cd dagger
