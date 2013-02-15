dbc4bf0191ca02d52963b122d20785e44588715e
Kristin Boyer <boyerkr@umich.edu>
2013-02-15 00:48:09 -0800

    youtube videos

--- MGoBlog/src/main/java/com/atami/mgodroid/util/MobileHTMLUtil.java
+++ MGoBlog/src/main/java/com/atami/mgodroid/util/MobileHTMLUtil.java
@@ -1,79 +1,78 @@
 package com.atami.mgodroid.util;
 
-
 import org.jsoup.Jsoup;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.jsoup.parser.Tag;
 import org.jsoup.safety.Whitelist;
 
+import android.util.Log;
+
 public class MobileHTMLUtil {
 
-    /**
-     * Cleans HTML fragments and makes them more user
-     * fridnly for mobile devices
-     * @param html an HTML fragment
-     * @return a cleaned HTML body
-     */
-    public static String clean(String html){
+	/**
+	 * Cleans HTML fragments and makes them more user fridnly for mobile devices
+	 * 
+	 * @param html
+	 *            an HTML fragment
+	 * @return a cleaned HTML body
+	 */
+	public static String clean(String html) {
 
-        html = Jsoup.clean(html, Whitelist.basicWithImages());
-        Document doc = Jsoup.parseBodyFragment(html);
+		Document doc = Jsoup.parseBodyFragment(html);
+		Elements videos = doc.select("iframe[src~=(youtube\\.com)], object[data~=(youtube\\.com)], embed[src~=(youtube\\.com)]");
+		
+		for (Element video : videos) replaceYouTube(video);
+		
+		// have to whitelist AFTER replacing videos with images so that 
+		// the videos don't get "cleaned" out
+		html = Jsoup.clean(doc.toString(), Whitelist.basicWithImages());
+		doc = Jsoup.parseBodyFragment(html);
+		
+		// Add MGoBlog css
+		doc.head().append("<link rel=\"stylesheet\" type=\"text/css\" href=\"node_body.css\"></style>");
+		
+		// TODO: Find plain text links and wrap them in an anchor tag
+		return doc.toString().replace("<p>&nbsp;</p>", "").trim();
+	}
 
-        //Add MGoBlog css before storing
-        Element headNode = doc.head();
-        headNode.append("<link rel=\"stylesheet\" type=\"text/css\" href=\"node_body.css\"></style>");
+	public static CharSequence trimTrailingWhitespace(CharSequence source) {
 
-        //Iterate through iframes, replace Youtube embeds with
-        //a thumbnail that links to Youtube
-        for (Element iframe : doc.select("iframe")) {
-            if (iframe.attr("src").contains("youtube")) {
-                Element div = new Element(Tag.valueOf("div"), "").attr("class", "video");
-                Element thumbnail = new Element(Tag.valueOf("a"), "").attr("href", iframe.attr("src"));
-                String videoID = iframe.attr("src").replaceFirst(".*/([^/?]+).*", "$1");
-                String thumbnailURL = String.format("http://img.youtube.com/vi/%s/0.jpg", videoID);
-                Element img = new Element(Tag.valueOf("img"), "")
-                        .attr("src", "play_button.png")
-                        .attr("style", "background:URL(" + thumbnailURL + ")");
-                thumbnail.appendChild(img);
-                div.appendChild(thumbnail);
-                iframe.replaceWith(div);
-            }
-        }
+		if (source == null)
+			return "";
 
-        for (Element embed : doc.select("embed")) {
-            if (embed.attr("src").contains("youtube")) {
-                Element div = new Element(Tag.valueOf("div"), "").attr("class", "video");
-                Element thumbnail = new Element(Tag.valueOf("a"), "").attr("href", embed.attr("src"));
-                String src = embed.attr("src").substring(0, embed.attr("src").lastIndexOf("?"));
-                String videoID = src.replaceFirst(".*/([^/?]+).*", "$1");
-                String thumbnailURL = String.format("http://img.youtube.com/vi/%s/0.jpg", videoID);
-                Element img = new Element(Tag.valueOf("img"), "")
-                        .attr("src", "play_button.png")
-                        .attr("style", "background:URL(" + thumbnailURL + ")");
-                thumbnail.appendChild(img);
-                div.appendChild(thumbnail);
-                embed.replaceWith(div);
-            }
+		int i = source.length();
 
-        }
+		// loop back to the first non-whitespace character
+		while (--i >= 0 && Character.isWhitespace(source.charAt(i))) {
+		}
 
-        //TODO: Find plain text links and wrap them in an anchor tag
+		return source.subSequence(0, i + 1);
+	}
 
-        return doc.toString().replace("<p>&nbsp;</p>", "").trim();
-    }
-    
-    public static CharSequence trimTrailingWhitespace(CharSequence source) {
+	public static void replaceYouTube(Element elt) {
+		if (elt.attr("src").contains("youtube")) {
+			Element div = new Element(Tag.valueOf("div"), "").attr("class",
+					"video");
+			Element thumbnail = new Element(Tag.valueOf("a"), "").attr("href",
+					elt.attr("src"));
+			
+			int len = elt.attr("src").lastIndexOf("?");
+			if(len <= 0) len = elt.attr("src").length();
+			
+			String src = elt.attr("src").substring(0, len);
+			String videoID = src.replaceFirst(".*/([^/?]+).*", "$1");
+			String thumbnailURL = String.format(
+					"http://img.youtube.com/vi/%s/0.jpg", videoID);
+			
+			Element img = new Element(Tag.valueOf("img"), "").attr("src",
+					"http://i.imgur.com/RRYojjC.png").attr("style",
+					"background:URL(" + thumbnailURL + ")");
 
-	    if(source == null)
-	        return "";
-
-	    int i = source.length();
-
-	    // loop back to the first non-whitespace character
-	    while(--i >= 0 && Character.isWhitespace(source.charAt(i))) {
-	    }
-
-	    return source.subSequence(0, i+1);
+			thumbnail.appendChild(img);
+			div.appendChild(thumbnail);
+			elt.replaceWith(div);
+		}
 	}
 }
