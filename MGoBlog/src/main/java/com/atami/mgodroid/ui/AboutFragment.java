3046ad6775b055737ec7b9013ac45db01e9edf0d
Kristin Boyer <boyerkr@umich.edu>
2013-02-14 23:10:52 -0800

    about pg finished

--- MGoBlog/src/main/java/com/atami/mgodroid/ui/AboutFragment.java
+++ MGoBlog/src/main/java/com/atami/mgodroid/ui/AboutFragment.java
@@ -1,9 +1,12 @@
 package com.atami.mgodroid.ui;
 
 import android.os.Bundle;
+import android.text.Html;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
 import com.atami.mgodroid.R;
 import com.atami.mgodroid.ui.base.BaseFragment;
 
@@ -26,14 +29,19 @@ public class AboutFragment extends BaseFragment {
 	public void onActivityCreated(Bundle savedInstanceState) {
 		super.onActivityCreated(savedInstanceState);
 
-		
 	}
-	
+
 	@Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container, 
-        Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.about, container, false);
-    }
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		View v = inflater.inflate(R.layout.about, container, false);
+
+		TextView tv = (TextView) v.findViewById(R.id.about);
+		tv.setText(Html
+				.fromHtml("MGoBlog covers football, basketball, hockey, baseball, lacrosse, recruiting, and much, much more. This app was designed by two University of Michigan Alumni, Sean O'Neil and Kristin Boyer. " +
+						"It is open source and can be checked out on <a href=\"https://github.com/SeanPONeil/MGoBlog\">GitHub</a>."));
+
+		return v;
+	}
 
 }
