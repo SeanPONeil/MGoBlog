4f43be665a4951166d13f8c2142b4c788817efe3
Sean O'Neil <oneilse14@gmail.com>
2013-02-18 11:55:46 -0500

    Don't strip out table tags

--- MGoBlog/src/main/java/com/atami/mgodroid/util/MobileHTMLUtil.java
+++ MGoBlog/src/main/java/com/atami/mgodroid/util/MobileHTMLUtil.java
@@ -7,73 +7,70 @@ import org.jsoup.select.Elements;
 import org.jsoup.parser.Tag;
 import org.jsoup.safety.Whitelist;
 
-import android.util.Log;
-
 public class MobileHTMLUtil {
 
-	/**
-	 * Cleans Node HTML fragments and makes them more user fridnly for mobile devices
-	 * 
-	 * @param html
-	 *            an HTML fragment
-	 * @return a cleaned HTML body
-	 */
-	public static String cleanNode(String html) {
+    /**
+     * Cleans Node HTML fragments and makes them more user fridnly for mobile devices
+     *
+     * @param html an HTML fragment
+     * @return a cleaned HTML body
+     */
+    public static String cleanNode(String html) {
 
-		Document doc = Jsoup.parseBodyFragment(html);
-		Elements videos = doc
-				.select("iframe[src~=(youtube\\.com)], embed[src~=(youtube\\.com)]");
+        Document doc = Jsoup.parseBodyFragment(html);
+        Elements videos = doc
+                .select("iframe[src~=(youtube\\.com)], embed[src~=(youtube\\.com)]");
 
-		// start by adding placeholder <a> tags to keep videos before cleanNode
-		for (Element video : videos) {
-			Element div = new Element(Tag.valueOf("div"), "").attr("class",
-					"video");
-			Element thumbnail = new Element(Tag.valueOf("a"), "").attr("href",
-					video.attr("src"));
+        // start by adding placeholder <a> tags to keep videos before cleanNode
+        for (Element video : videos) {
+            Element div = new Element(Tag.valueOf("div"), "").attr("class",
+                    "video");
+            Element thumbnail = new Element(Tag.valueOf("a"), "").attr("href",
+                    video.attr("src"));
 
-			div.appendChild(thumbnail);
-			video.replaceWith(div);
-		}
+            div.appendChild(thumbnail);
+            video.replaceWith(div);
+        }
 
-		// have to whitelist AFTER replacing videos with images so that
-		// the videos don't get "cleaned" out
-		html = Jsoup.clean(doc.toString(), Whitelist.basicWithImages());
-		doc = Jsoup.parseBodyFragment(html);
+        // have to whitelist AFTER replacing videos with images so that
+        // the videos don't get "cleaned" out
+        html = Jsoup.clean(doc.toString(), Whitelist.relaxed());
+        doc = Jsoup.parseBodyFragment(html);
 
-		videos = doc.select("a[href~=(youtube\\.com)]");
+        videos = doc.select("a[href~=(youtube\\.com)]");
 
-		// add images with styling to the <a> tags that point to youtube
-		for (Element video : videos) {
-			
-			int len = video.attr("href").lastIndexOf("?");
+        // add images with styling to the <a> tags that point to youtube
+        for (Element video : videos) {
 
-			if (len <= 0) {
-				len = video.attr("href").length();
-			}
+            int len = video.attr("href").lastIndexOf("?");
 
-			String src = video.attr("href").substring(0, len);
+            if (len <= 0) {
+                len = video.attr("href").length();
+            }
 
-			String videoID = src.replaceFirst(".*/([^/?]+).*", "$1");
-			String thumbnailURL = String.format(
-					"http://img.youtube.com/vi/%s/0.jpg", videoID);
+            String src = video.attr("href").substring(0, len);
 
-			Element img = new Element(Tag.valueOf("img"), "")
-					.attr("src",
-							"https://raw.github.com/SeanPONeil/MGoBlog/master/MGoBlog/assets/play_button.png")
-					.attr("style", "background:URL(" + thumbnailURL + ")");
+            String videoID = src.replaceFirst(".*/([^/?]+).*", "$1");
+            String thumbnailURL = String.format(
+                    "http://img.youtube.com/vi/%s/0.jpg", videoID);
 
-			video.appendChild(img);
-		}
+            Element img = new Element(Tag.valueOf("img"), "")
+                    .attr("src",
+                            "https://raw.github.com/SeanPONeil/MGoBlog/master/MGoBlog/assets/play_button.png")
+                    .attr("style", "background:URL(" + thumbnailURL + ")");
 
-		// Add MGoBlog css
-		doc.head()
-				.append("<link rel=\"stylesheet\" type=\"text/css\" href=\"node_body.css\"></style>");
+            video.appendChild(img);
+        }
 
-		// TODO: Find plain text links and wrap them in an anchor tag
-		return doc.toString().replace("<p>&nbsp;</p>", "").trim();
-	}
+        // Add MGoBlog css
+        doc.head()
+                .append("<link rel=\"stylesheet\" type=\"text/css\" href=\"node_body.css\"></style>");
 
-    public static String addLinkToBody(String html, String link){
+        // TODO: Find plain text links and wrap them in an anchor tag
+        return doc.toString().replace("<p>&nbsp;</p>", "").trim();
+    }
+
+    public static String addLinkToBody(String html, String link) {
         Document doc = Jsoup.parse(html);
         Element href = new Element(Tag.valueOf("a"), "");
         href.attr("href", link);
@@ -81,33 +78,33 @@ public class MobileHTMLUtil {
         doc.body().appendChild(href);
         return doc.toString();
     }
-	
-	public static String cleanComments(String html) {
 
-		Document doc = Jsoup.parseBodyFragment(html);
-		Elements media = doc.select("img[src], iframe[src], embed[src]");
+    public static String cleanComments(String html) {
 
-		for(Element m : media){
-			Element p = new Element(Tag.valueOf("p"), "");
-			Element e = new Element(Tag.valueOf("a"), "").attr("href",
-					m.attr("src"));
-			e.text(m.attr("src"));
+        Document doc = Jsoup.parseBodyFragment(html);
+        Elements media = doc.select("img[src], iframe[src], embed[src]");
 
-			p.appendChild(e);
-			m.replaceWith(p);
-		}
+        for (Element m : media) {
+            Element p = new Element(Tag.valueOf("p"), "");
+            Element e = new Element(Tag.valueOf("a"), "").attr("href",
+                    m.attr("src"));
+            e.text(m.attr("src"));
 
-		// have to whitelist AFTER replacing videos with images so that
-		// the videos don't get "cleaned" out
-		html = Jsoup.clean(doc.toString(), Whitelist.basicWithImages());
-		doc = Jsoup.parseBodyFragment(html);
+            p.appendChild(e);
+            m.replaceWith(p);
+        }
 
-		// Add MGoBlog css
-		doc.head()
-				.append("<link rel=\"stylesheet\" type=\"text/css\" href=\"node_body.css\"></style>");
+        // have to whitelist AFTER replacing videos with images so that
+        // the videos don't get "cleaned" out
+        html = Jsoup.clean(doc.toString(), Whitelist.relaxed());
+        doc = Jsoup.parseBodyFragment(html);
 
-		// TODO: Find plain text links and wrap them in an anchor tag
-		return doc.toString().replace("<p>&nbsp;</p>", "").trim();
-	}
+        // Add MGoBlog css
+        doc.head()
+                .append("<link rel=\"stylesheet\" type=\"text/css\" href=\"node_body.css\"></style>");
+
+        // TODO: Find plain text links and wrap them in an anchor tag
+        return doc.toString().replace("<p>&nbsp;</p>", "").trim();
+    }
 
 }
